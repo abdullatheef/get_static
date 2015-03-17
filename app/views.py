69743e382251ac6eacf1dea1fa8d89d0e70a2957
Latheef <latheefvkpadi@gmail.com>
2015-03-17 10:59:59 +0530

    added files

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+import simplejson
+# Create your views here.
+
+@csrf_exempt
+def home(request):
+	try:
+		if request.method == "POST":
+			return HttpResponse(
+                simplejson.dumps(
+                    {"files": [
+                      {
+                        "name": "77464_537620316255351_1441662113_o.jpg.jpg",
+                        "size": 902604,
+                        "error": "Filetype not alloweddd"
+                      }
+                    ]}
+                )
+			)
+	except:
+		pass
+	return render(request, 'home.html', {})
